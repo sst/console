3149b2502950f05c6baf9bfd6dc7c021308dfe44
Jay V <air@live.ca>
2023-06-12 14:43:38 -0400

    Stories for buttons

--- packages/web/workspace/src/pages/design/index.tsx
+++ packages/web/workspace/src/pages/design/index.tsx
@@ -2,6 +2,7 @@ import { Row } from "$/ui/layout";
 import { styled } from "@macaron-css/solid";
 import { theme } from "$/ui/theme";
 import { IconClipboard } from "$/ui/icons";
+import { Button } from "$/ui/button";
 import { IconNodeRuntime } from "$/ui/icons/custom";
 import {
   Child,
@@ -12,8 +13,9 @@ import {
   ChildTitle,
   ChildTitleLink,
 } from "$/pages//workspace/stage/resources";
+import { JSX } from "solid-js";
 
-const Component = styled("div", {
+const ComponentRoot = styled("div", {
   base: {
     padding: theme.space[4],
   },
@@ -28,9 +30,19 @@ const ComponentName = styled("h1", {
   },
 });
 
-const Variant = styled("div", {
+const ComponentVariants = styled("div", {
   base: {
-    marginBottom: theme.space[4],
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.space[4],
+  },
+});
+
+const VariantRoot = styled("div", {
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.space[4],
   },
 });
 
@@ -38,100 +50,185 @@ const VariantName = styled("h2", {
   base: {
     fontSize: "0.8125rem",
     textTransform: "uppercase",
-    paddingBottom: theme.space[4],
     fontFamily: theme.fonts.heading,
   },
 });
 
 const VariantContent = styled("div", {
   base: {
+    display: "flex",
+    gap: theme.space[2],
     border: `1px solid ${theme.color.divider.base}`,
     padding: theme.space[4],
     borderRadius: theme.borderRadius,
   },
 });
 
+const VariantThemes = styled("div", {
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.space[2],
+  },
+});
+
+interface ComponentProps {
+  name: string;
+  children: JSX.Element;
+}
+
+interface VariantProps {
+  themes?: boolean;
+  name: string;
+  children: JSX.Element;
+}
+
+function Component(props: ComponentProps) {
+  return (
+    <ComponentRoot>
+      <ComponentName>{props.name}</ComponentName>
+      <ComponentVariants>{props.children}</ComponentVariants>
+    </ComponentRoot>
+  );
+}
+
+function Variant(props: VariantProps) {
+  return (
+    <VariantRoot>
+      <VariantName>{props.name}</VariantName>
+      <VariantContent>{props.children}</VariantContent>
+    </VariantRoot>
+  );
+}
+const OverflowSpan = styled("span", {
+  base: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+  },
+});
+
 export function Design() {
   return (
     <>
-      <Component>
-        <ComponentName>Resources</ComponentName>
-        <Variant>
-          <VariantName>Default</VariantName>
-          <VariantContent>
-            <Child>
-              <Row space="2" vertical="center">
-                <ChildTag>OPTIONS</ChildTag>
-                <ChildTitleLink>/notes/settings</ChildTitleLink>
-              </Row>
-              <Row shrink={false} space="3" vertical="center">
-                <ChildDetail>11.2 MB</ChildDetail>
-                <ChildIcon>
-                  <IconNodeRuntime />
-                </ChildIcon>
-                <ChildExtra>us-east-1</ChildExtra>
-              </Row>
-            </Child>
-          </VariantContent>
+      <Component name="Button">
+        <Variant name="Primary">
+          <Button color="primary">Button</Button>
+          <Button data-state-hover color="primary">
+            Button
+          </Button>
+          <Button data-state-active color="primary">
+            Button
+          </Button>
+          <Button disabled color="primary">
+            Button
+          </Button>
         </Variant>
-        <Variant>
-          <VariantName>Overflow</VariantName>
-          <VariantContent>
-            <Child>
-              <Row space="2" vertical="center">
-                <ChildTag>OPTIONS</ChildTag>
-                <ChildTitleLink>
-                  /notes/settings/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long
-                </ChildTitleLink>
-              </Row>
-              <Row shrink={false} space="3" vertical="center">
-                <ChildDetail>11.2 MB</ChildDetail>
-                <ChildIcon>
-                  <IconNodeRuntime />
-                </ChildIcon>
-                <ChildExtra>us-east-1</ChildExtra>
-              </Row>
-            </Child>
-          </VariantContent>
+        <Variant name="Danger">
+          <Button color="danger">Button</Button>
+          <Button data-state-hover color="danger">
+            Button
+          </Button>
+          <Button data-state-active color="danger">
+            Button
+          </Button>
+          <Button disabled color="danger">
+            Button
+          </Button>
+        </Variant>
+        <Variant name="Secondary">
+          <Button color="secondary">Button</Button>
+          <Button data-state-hover color="secondary">
+            Button
+          </Button>
+          <Button data-state-active color="secondary">
+            Button
+          </Button>
+          <Button disabled color="secondary">
+            Button
+          </Button>
         </Variant>
       </Component>
-      <Component>
-        <ComponentName>Outputs</ComponentName>
-        <Variant>
-          <VariantName>Default</VariantName>
-          <VariantContent>
-            <Child>
-              <Row shrink={false}>
-                <ChildTitle>ApiEndpoint</ChildTitle>
-              </Row>
-              <Row vertical="center" space="2">
-                <ChildDetail>
-                  https://mwismf5e9l.execute-api.us-east-1.amazonaws.com/prod
-                </ChildDetail>
-                <ChildIcon>
-                  <IconClipboard />
-                </ChildIcon>
-              </Row>
-            </Child>
-          </VariantContent>
+      <Component name="Row">
+        <Variant name="Default">
+          <Row space="2" vertical="center">
+            <span>Label</span>
+            <span>Value</span>
+          </Row>
         </Variant>
-        <Variant>
-          <VariantName>Overflow</VariantName>
-          <VariantContent>
-            <Child>
-              <Row shrink={false}>
-                <ChildTitle>ApiEndpoint</ChildTitle>
-              </Row>
-              <Row vertical="center" space="2">
-                <ChildDetail>
-                  https://mwismf5e9l.execute-api.us-east-1.amazonaws.com/prod/with/an/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long
-                </ChildDetail>
-                <ChildIcon>
-                  <IconClipboard />
-                </ChildIcon>
-              </Row>
-            </Child>
-          </VariantContent>
+        <Variant name="Overflow">
+          <Row space="2" vertical="center">
+            <span>Label</span>
+            <OverflowSpan>
+              /notes/settings/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long
+            </OverflowSpan>
+          </Row>
+        </Variant>
+      </Component>
+      <Component name="Resources">
+        <Variant name="Default">
+          <Child>
+            <Row space="2" vertical="center">
+              <ChildTag>OPTIONS</ChildTag>
+              <ChildTitleLink>/notes/settings</ChildTitleLink>
+            </Row>
+            <Row shrink={false} space="3" vertical="center">
+              <ChildDetail>11.2 MB</ChildDetail>
+              <ChildIcon>
+                <IconNodeRuntime />
+              </ChildIcon>
+              <ChildExtra>us-east-1</ChildExtra>
+            </Row>
+          </Child>
+        </Variant>
+        <Variant name="Overflow">
+          <Child>
+            <Row space="2" vertical="center">
+              <ChildTag>OPTIONS</ChildTag>
+              <ChildTitleLink>
+                /notes/settings/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long
+              </ChildTitleLink>
+            </Row>
+            <Row shrink={false} space="3" vertical="center">
+              <ChildDetail>11.2 MB</ChildDetail>
+              <ChildIcon>
+                <IconNodeRuntime />
+              </ChildIcon>
+              <ChildExtra>us-east-1</ChildExtra>
+            </Row>
+          </Child>
+        </Variant>
+      </Component>
+      <Component name="Outputs">
+        <Variant name="Default">
+          <Child>
+            <Row shrink={false}>
+              <ChildTitle>ApiEndpoint</ChildTitle>
+            </Row>
+            <Row vertical="center" space="2">
+              <ChildDetail>
+                https://mwismf5e9l.execute-api.us-east-1.amazonaws.com/prod
+              </ChildDetail>
+              <ChildIcon>
+                <IconClipboard />
+              </ChildIcon>
+            </Row>
+          </Child>
+        </Variant>
+        <Variant name="Overflow">
+          <Child>
+            <Row shrink={false}>
+              <ChildTitle>ApiEndpoint</ChildTitle>
+            </Row>
+            <Row vertical="center" space="2">
+              <ChildDetail>
+                https://mwismf5e9l.execute-api.us-east-1.amazonaws.com/prod/with/an/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long/absurdly/long/path/that/should/overflow/because/its/way/too/long
+              </ChildDetail>
+              <ChildIcon>
+                <IconClipboard />
+              </ChildIcon>
+            </Row>
+          </Child>
         </Variant>
       </Component>
     </>
--- packages/web/workspace/src/ui/button.tsx
+++ packages/web/workspace/src/ui/button.tsx
@@ -1,6 +1,36 @@
 import { styled } from "@macaron-css/solid";
 import { theme } from "./theme";
 
+const primaryHover = {
+  borderColor: theme.color.button.primary.hover.border,
+  backgroundColor: theme.color.button.primary.hover.color,
+};
+const primaryActive = {
+  backgroundColor: theme.color.button.primary.active,
+  transform: "translateY(1px)",
+  borderColor: "transparent",
+  boxShadow: "none",
+};
+const secondaryHover = {
+  borderColor: theme.color.button.secondary.hover.border,
+  backgroundColor: theme.color.button.secondary.hover.color,
+};
+const secoondaryActive = {
+  backgroundColor: theme.color.button.secondary.active,
+  transform: "translateY(1px)",
+  boxShadow: "none",
+};
+const dangerHover = {
+  borderColor: theme.color.button.danger.hover.border,
+  backgroundColor: theme.color.button.danger.hover.color,
+};
+const dangerActive = {
+  backgroundColor: theme.color.button.danger.active,
+  transform: "translateY(1px)",
+  borderColor: "transparent",
+  boxShadow: "none",
+};
+
 export const Button = styled("button", {
   base: {
     appearance: "none",
@@ -26,20 +56,16 @@ export const Button = styled("button", {
         borderColor: theme.color.button.primary.border,
         boxShadow: theme.color.shadow.button.accent,
         color: theme.color.text.primary.accent,
-        ":hover": {
-          borderColor: theme.color.button.primary.hover.border,
-          backgroundColor: theme.color.button.primary.hover.color,
-        },
-        ":active": {
-          backgroundColor: theme.color.button.primary.active,
-          transform: "translateY(1px)",
-          borderColor: "transparent",
-          boxShadow: "none",
-        },
         ":disabled": {
           borderColor: "transparent",
           boxShadow: "none",
           opacity: theme.color.button.primary.disabled.opacity,
+        },
+        selectors: {
+          "&:hover": primaryHover,
+          "&:active": primaryActive,
+          "&[data-state-hover]": primaryHover,
+          "&[data-state-active]": primaryActive,
         },
       },
       secondary: {
@@ -47,18 +73,15 @@ export const Button = styled("button", {
         borderColor: theme.color.button.secondary.border,
         boxShadow: theme.color.shadow.button.base,
         color: theme.color.text.primary.surface,
-        ":hover": {
-          borderColor: theme.color.button.secondary.hover.border,
-          backgroundColor: theme.color.button.secondary.hover.color,
-        },
-        ":active": {
-          backgroundColor: theme.color.button.secondary.active,
-          transform: "translateY(1px)",
-          boxShadow: "none",
-        },
         ":disabled": {
           boxShadow: "none",
           opacity: theme.color.button.secondary.disabled.opacity,
+        },
+        selectors: {
+          "&:hover": secondaryHover,
+          "&:active": secoondaryActive,
+          "&[data-state-hover]": secondaryHover,
+          "&[data-state-active]": secoondaryActive,
         },
       },
       danger: {
@@ -66,20 +89,16 @@ export const Button = styled("button", {
         borderColor: theme.color.button.danger.border,
         boxShadow: theme.color.shadow.button.danger,
         color: theme.color.text.primary.danger,
-        ":hover": {
-          borderColor: theme.color.button.danger.hover.border,
-          backgroundColor: theme.color.button.danger.hover.color,
-        },
-        ":active": {
-          backgroundColor: theme.color.button.danger.active,
-          transform: "translateY(1px)",
-          borderColor: "transparent",
-          boxShadow: "none",
-        },
         ":disabled": {
           borderColor: "transparent",
           boxShadow: "none",
           opacity: theme.color.button.danger.disabled.opacity,
+        },
+        selectors: {
+          "&:hover": dangerHover,
+          "&:active": dangerActive,
+          "&[data-state-hover]": dangerHover,
+          "&[data-state-active]": dangerActive,
         },
       },
     },
