32669db0a2e11c6a4ab6e3e22ee94f441a20497c
Sock, the dev <sockthedev@gmail.com>
2023-06-16 15:59:34 +0800

    Adds resource to resolve IoT Host

--- stacks/web.ts
+++ stacks/web.ts
@@ -1,12 +1,14 @@
-import { Api, StackContext, StaticSite, use } from "sst/constructs";
+import { StackContext, StaticSite, use } from "sst/constructs";
 import { API } from "./api";
 import { Auth } from "./auth";
 import { DNS } from "./dns";
+import { Realtime } from "./realtime";
 
 export function Web({ stack }: StackContext) {
   const dns = use(DNS);
   const api = use(API);
   const auth = use(Auth);
+  const realtime = use(Realtime)
 
   const workspace = new StaticSite(stack, "workspace", {
     path: "./packages/web/workspace",
@@ -19,10 +21,7 @@ export function Web({ stack }: StackContext) {
     environment: {
       VITE_API_URL: api.customDomainUrl || api.url,
       VITE_AUTH_URL: auth.url,
-      VITE_IOT_HOST:
-        stack.stage === "production"
-          ? "aebfn7iaj9d1x-ats.iot.us-east-1.amazonaws.com"
-          : "a39w1dev1zzfpb-ats.iot.us-east-1.amazonaws.com",
+      VITE_IOT_HOST: realtime.endpointAddress,
       VITE_STAGE: stack.stage,
     },
   });
