da01d603bd0b7638cf56e72f4edbb57f77c65c1d
Dax Raad <d@ironbay.co>
2023-06-19 17:36:43 -0400

    scrap

--- packages/scripts/src/scrap.ts
+++ packages/scripts/src/scrap.ts
@@ -2,21 +2,16 @@ import { Workspace } from "@console/core/workspace";
 import { User } from "@console/core/user";
 import { provideActor } from "@console/core/actor";
 
-{
-  const workspaceID = await Workspace.create({
-    slug: "goodvisit",
-  });
+provideActor({
+  type: "system",
+  properties: {
+    workspaceID: "cjl2fmobui506ctxs8bdvsez",
+  },
+});
 
-  provideActor({
-    type: "system",
-    properties: {
-      workspaceID,
-    },
-  });
-
-  await User.create({
-    email: "mail@thdxr.com",
-  });
-}
+await User.create({
+  id: "pnludxtej0qprh7kk10saipp",
+  email: "mail@thdxr.com",
+});
 
 export {};
