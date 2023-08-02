54288fa3db340552c5378899846636f5ba1ec1ce
Dax Raad <d@ironbay.co>
2023-08-02 11:47:33 -0400

    log updated event

--- packages/functions/src/events/app-stage-updated.ts
+++ packages/functions/src/events/app-stage-updated.ts
@@ -4,6 +4,7 @@ import { AWS } from "@console/core/aws";
 import { EventHandler } from "sst/node/event-bus";
 
 export const handler = EventHandler(Stage.Events.Updated, async (evt) => {
+  console.log(evt);
   provideActor(evt.metadata.actor);
   const { credentials } = await Stage.assumeRole(evt.properties.stageID);
   if (!credentials) return;
