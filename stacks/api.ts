94dac6dafb3c0ba2499ad585ef1d969ede93bdb0
Dax Raad <d@ironbay.co>
2023-08-07 18:09:03 -0400

    switch back to error

--- stacks/api.ts
+++ stacks/api.ts
@@ -83,7 +83,7 @@ export function API({ stack, app }: StackContext) {
     routes: {
       "POST /replicache/pull": "packages/functions/src/replicache/pull.handler",
       "POST /replicache/push": "packages/functions/src/replicache/push.handler",
-      "GET /error2": {
+      "GET /error": {
         type: "function",
         function: {
           handler: "packages/functions/src/error.handler",
