f821e38e1d9bb84554617b48c085d3ed2d406f7d
Dax Raad <d@ironbay.co>
2023-08-09 20:51:54 -0400

    push

--- stacks/dns.ts
+++ stacks/dns.ts
@@ -1,15 +1,21 @@
-import { HostedZone } from "aws-cdk-lib/aws-route53";
+import { CnameRecord, HostedZone } from "aws-cdk-lib/aws-route53";
 import { StackContext } from "sst/constructs";
 
 const PRODUCTION = "console.sst.dev";
-const DEV = "console.dev.sst.dev";
+const DEV = "dev.console.sst.dev";
 
 export function DNS(ctx: StackContext) {
   if (ctx.stack.stage === "production") {
+    const zone = new HostedZone(ctx.stack, "zone", {
+      zoneName: PRODUCTION,
+    });
+    new CnameRecord(ctx.stack, "old", {
+      zone,
+      recordName: "old",
+      domainName: "sst-console.netlify.app",
+    });
     return {
-      zone: new HostedZone(ctx.stack, "zone", {
-        zoneName: PRODUCTION,
-      }),
+      zone,
       domain: PRODUCTION,
     };
   }
