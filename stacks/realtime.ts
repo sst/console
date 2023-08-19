af43d59ed5c685de4fe6baa0c7af783fc3c9bca5
Sock, the dev <114172997+sockthedev@users.noreply.github.com>
2023-08-19 09:26:54 +0800

    Adds resource to resolve IoT Host (#1)
    Co-authored-by: Dax <d@ironbay.co>

--- stacks/realtime.ts
+++ stacks/realtime.ts
@@ -1,9 +1,10 @@
-import { ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { StackContext, use } from "sst/constructs";
 import { CfnAuthorizer } from "aws-cdk-lib/aws-iot";
 import { Function } from "sst/constructs";
 import { Auth } from "./auth";
 import { Secrets } from "./secrets";
+import { AwsCustomResource, AwsCustomResourcePolicy, AwsSdkCall, PhysicalResourceId } from "aws-cdk-lib/custom-resources";
 
 export function Realtime(ctx: StackContext) {
   const auth = use(Auth);
@@ -30,5 +31,49 @@ export function Realtime(ctx: StackContext) {
     action: "lambda:InvokeFunction",
   });
 
-  return {};
+  const describeEndpointRole = new Role(ctx.stack, "LambdaRole", {
+    assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+  })
+
+  describeEndpointRole.addManagedPolicy(
+    ManagedPolicy.fromAwsManagedPolicyName(
+      "service-role/AWSLambdaBasicExecutionRole",
+    ),
+  )
+
+  describeEndpointRole.addToPolicy(
+    new PolicyStatement({
+      resources: ["*"],
+      actions: ["iot:DescribeEndpoint"],
+    }),
+  )
+
+  const describeEndpointSdkCall: AwsSdkCall = {
+    service: "Iot",
+    action: "describeEndpoint",
+    parameters: {
+      endpointType: "iot:Data-ATS",
+    },
+    region: ctx.stack.region,
+    physicalResourceId: PhysicalResourceId.of(
+      "IoTEndpointDescription",
+    ),
+  }
+
+  const describeEndpointResource = new AwsCustomResource(
+    ctx.stack,
+    "Resource",
+    {
+      onCreate: describeEndpointSdkCall,
+      onUpdate: describeEndpointSdkCall,
+      policy: AwsCustomResourcePolicy.fromSdkCalls({
+        resources: AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+      role: describeEndpointRole,
+    },
+  )
+
+  return {
+    endpointAddress: describeEndpointResource.getResponseField("endpointAddress"),
+  }
 }
