6c9a63ff9a3a0e5f2afac4a0e3907544e0c06ea1
Dax Raad <d@ironbay.co>
2023-08-25 11:17:30 -0400

    slow down region scanning

--- packages/core/src/aws/account.ts
+++ packages/core/src/aws/account.ts
@@ -258,116 +258,114 @@ export const integrate = zod(
     const r = await regions(input.credentials);
     console.log("regions", r);
 
-    await Promise.all(
-      r.map(async (region) => {
-        const config = {
-          credentials: input.credentials,
-          region: region!,
-        };
-        console.log("integrating region", region);
+    for (const region of r) {
+      const config = {
+        credentials: input.credentials,
+        region: region!,
+      };
+      console.log("integrating region", region);
 
-        const b = await bootstrap(config);
-        if (!b) return;
+      const b = await bootstrap(config);
+      if (!b) return;
 
-        const s3 = new S3Client(config);
-        const eb = new EventBridgeClient(config);
+      const s3 = new S3Client(config);
+      const eb = new EventBridgeClient(config);
 
-        console.log(region, "found sst bucket", b.bucket);
+      console.log(region, "found sst bucket", b.bucket);
 
-        const result = await s3
-          .send(
-            new PutBucketNotificationConfigurationCommand({
-              Bucket: b.bucket,
-              NotificationConfiguration: {
-                EventBridgeConfiguration: {},
+      const result = await s3
+        .send(
+          new PutBucketNotificationConfigurationCommand({
+            Bucket: b.bucket,
+            NotificationConfiguration: {
+              EventBridgeConfiguration: {},
+            },
+          })
+        )
+        .catch(() => {});
+      if (!result) {
+        console.log(region, "failed to update bucket notification");
+        return;
+      }
+      console.log(region, "updated bucket notifications");
+
+      await eb.send(
+        new PutRuleCommand({
+          Name: "SSTConsole" + suffix,
+          State: "ENABLED",
+          EventPattern: JSON.stringify({
+            source: ["aws.s3"],
+            detail: {
+              bucket: {
+                name: [b.bucket],
               },
-            })
-          )
-          .catch(() => {});
-        if (!result) {
-          console.log(region, "failed to update bucket notification");
-          return;
-        }
-        console.log(region, "updated bucket notifications");
+            },
+          }),
+        })
+      );
+      await eb.send(
+        new PutTargetsCommand({
+          Rule: "SSTConsole" + suffix,
+          Targets: [
+            {
+              Arn: process.env.EVENT_BUS_ARN,
+              Id: "SSTConsole",
+              RoleArn: role.Role!.Arn,
+            },
+          ],
+        })
+      );
+      console.log(region, "created eventbus rule");
 
-        await eb.send(
-          new PutRuleCommand({
-            Name: "SSTConsole" + suffix,
-            State: "ENABLED",
-            EventPattern: JSON.stringify({
-              source: ["aws.s3"],
-              detail: {
-                bucket: {
-                  name: [b.bucket],
-                },
-              },
-            }),
+      let token: string | undefined;
+      while (true) {
+        const list = await s3.send(
+          new ListObjectsV2Command({
+            Prefix: "stackMetadata",
+            Bucket: b.bucket,
+            ContinuationToken: token,
           })
         );
-        await eb.send(
-          new PutTargetsCommand({
-            Rule: "SSTConsole" + suffix,
-            Targets: [
-              {
-                Arn: process.env.EVENT_BUS_ARN,
-                Id: "SSTConsole",
-                RoleArn: role.Role!.Arn,
-              },
-            ],
-          })
+        const distinct = new Set(
+          list.Contents?.filter((item) => item.Key).map((item) =>
+            item.Key!.split("/").slice(0, 3).join("/")
+          ) || []
         );
-        console.log(region, "created eventbus rule");
 
-        let token: string | undefined;
-        while (true) {
-          const list = await s3.send(
-            new ListObjectsV2Command({
-              Prefix: "stackMetadata",
-              Bucket: b.bucket,
-              ContinuationToken: token,
-            })
-          );
-          const distinct = new Set(
-            list.Contents?.filter((item) => item.Key).map((item) =>
-              item.Key!.split("/").slice(0, 3).join("/")
-            ) || []
-          );
+        console.log("found", distinct);
+        for (const item of distinct) {
+          const [, appHint, stageHint] = item.split("/") || [];
+          if (!appHint || !stageHint) return;
+          const [, stageName] = stageHint?.split(".");
+          const [, appName] = appHint?.split(".");
+          if (!stageName || !appName) return;
+          await useTransaction(async () => {
+            let app = await App.fromName(appName).then((a) => a?.id);
+            if (!app)
+              app = await App.create({
+                name: appName,
+              });
 
-          console.log("found", distinct);
-          for (const item of distinct) {
-            const [, appHint, stageHint] = item.split("/") || [];
-            if (!appHint || !stageHint) return;
-            const [, stageName] = stageHint?.split(".");
-            const [, appName] = appHint?.split(".");
-            if (!stageName || !appName) return;
-            await useTransaction(async () => {
-              let app = await App.fromName(appName).then((a) => a?.id);
-              if (!app)
-                app = await App.create({
-                  name: appName,
-                });
-
-              let stage = await App.Stage.fromName({
-                appID: app,
+            let stage = await App.Stage.fromName({
+              appID: app,
+              name: stageName,
+              region,
+            }).then((s) => s?.id);
+            if (!stage) {
+              stage = await App.Stage.connect({
                 name: stageName,
-                region,
-              }).then((s) => s?.id);
-              if (!stage) {
-                stage = await App.Stage.connect({
-                  name: stageName,
-                  appID: app,
-                  region: config.region,
-                  awsAccountID: account.id,
-                });
-                await Replicache.poke();
-              }
-            });
-            console.log(region, "found", stageName, appName);
-          }
-          if (!list.ContinuationToken) break;
+                appID: app,
+                region: config.region,
+                awsAccountID: account.id,
+              });
+              await Replicache.poke();
+            }
+          });
+          console.log(region, "found", stageName, appName);
         }
-      })
-    );
+        if (!list.ContinuationToken) break;
+      }
+    }
 
     await db
       .update(awsAccount)
