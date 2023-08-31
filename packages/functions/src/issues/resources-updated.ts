dd7bfb41dc557fc9244f1d378abd8ff52fb369cc
Dax Raad <d@ironbay.co>
2023-08-31 13:59:17 -0400

    wire up resources updated

--- packages/functions/src/issues/resources-updated.ts
+++ packages/functions/src/issues/resources-updated.ts
@@ -1,10 +1,12 @@
 import { provideActor } from "@console/core/actor";
 import { App } from "@console/core/app";
+import { Issue } from "@console/core/issue";
 import { EventHandler } from "sst/node/event-bus";
 
 export const handler = EventHandler(
   App.Stage.Events.ResourcesUpdated,
   async (evt) => {
     provideActor(evt.metadata.actor);
+    await Issue.subscribe(evt.properties.stageID);
   }
 );
