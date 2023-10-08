8d0f7205f66fd94b82571e392ae997ae86b63412
Jay V <air@live.ca>
2023-10-08 16:06:44 +0100

    Working on emails

--- packages/web/mail/emails/issue.tsx
+++ packages/web/mail/emails/issue.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  Hr,
   Img,
   Row,
   Html,
@@ -14,24 +13,49 @@ import {
   Heading,
   Section,
   Container,
-  Text,
+  Hr as REHr,
+  Text as REText,
+  HrProps as REHrProps,
+  TextProps as RETextProps,
 } from "@react-email/components";
 
 const LOCAL_ASSETS_URL = "/static";
 
-const blackColor = "#1A1A2E";
-const dividerColor = "#D5D5D9";
-const backgroundColor = "#F0F0F1";
+const unit = 16;
+
+const GREY_COLOR = [
+  "#1A1A2E", //0
+  "#2F2F41", //1
+  "#444454", //2
+  "#585867", //3
+  "#6D6D7A", //4
+  "#82828D", //5
+  "#9797A0", //6
+  "#ACACB3", //7
+  "#C1C1C6", //8
+  "#D5D5D9", //9
+  "#EAEAEC", //10
+  "#FFFFFF", //11
+];
+
+const BLUE_COLOR = "#395C6B";
+const TEXT_COLOR = GREY_COLOR[0];
+const SECONDARY_COLOR = GREY_COLOR[5];
+const DIMMED_COLOR = GREY_COLOR[7];
+const DIVIDER_COLOR = GREY_COLOR[10];
+const BACKGROUND_COLOR = "#F0F0F1";
+const SURFACE_COLOR = DIVIDER_COLOR;
+const SURFACE_DIVIDER_COLOR = GREY_COLOR[9];
 
 const body = {
-  background: backgroundColor,
+  background: BACKGROUND_COLOR,
 };
 
 const container = {};
 
 const frame = {
-  padding: "32px",
-  border: `1px solid ${dividerColor}`,
+  padding: `${unit * 1.5}px`,
+  border: `1px solid ${SURFACE_DIVIDER_COLOR}`,
   background: "#FFF",
   borderRadius: "6px",
   boxShadow: `0 1px 2px rgba(0,0,0,0.03),
@@ -39,17 +63,145 @@ const frame = {
               0 2px 6px rgba(0,0,0,0.03)`,
 };
 
+const textColor = {
+  color: TEXT_COLOR,
+};
+
 const code = {
   fontFamily: "IBM Plex Mono, mono-space",
 };
 
+const tableCell = {
+  display: "table-cell",
+};
+
+const headingHr = {
+  margin: `${unit}px 0`,
+};
+
 const buttonPrimary = {
+  ...code,
   color: "#FFF",
   borderRadius: "4px",
-  padding: "12px 20px",
-  background: "#395C6B",
-  fontSize: "13px",
+  background: BLUE_COLOR,
+  fontSize: "12px",
+  fontWeight: 500,
 };
+
+const compactText = {
+  margin: "0 0 2px",
+};
+
+const issueBreadcrumb = {
+  fontSize: "14px",
+  color: SECONDARY_COLOR,
+};
+
+const issueBreadcrumbSeparator = {
+  color: DIVIDER_COLOR,
+};
+
+const issueHeading = {
+  ...code,
+  fontSize: "22px",
+  fontWeight: 500,
+};
+
+const sectionLabel = {
+  ...code,
+  ...compactText,
+  letterSpacing: "0.5px",
+  fontSize: "13px",
+  fontWeight: 500,
+  color: DIMMED_COLOR,
+};
+
+const stacktraceContainer = {
+  padding: `${unit * 0.75}px ${unit}px`,
+  borderRadius: "5px",
+  background: SURFACE_COLOR,
+};
+
+const stacktraceFrame = {
+  ...code,
+  fontSize: "13px",
+  color: DIMMED_COLOR,
+};
+const stacktraceFrameFileImportant = {
+  ...stacktraceFrame,
+  color: TEXT_COLOR,
+  fontWeight: 500,
+};
+const stacktraceFramePositionImportant = {
+  ...stacktraceFrame,
+  color: SECONDARY_COLOR,
+  fontWeight: 500,
+};
+const stacktraceFrameContext = {
+  ...code,
+  margin: "4px 0",
+  fontSize: "12px",
+  color: DIMMED_COLOR,
+};
+const stacktraceFrameContextImportant = {
+  ...stacktraceFrameContext,
+  color: TEXT_COLOR,
+  fontWeight: 500,
+};
+
+function insertWbr(
+  input: string,
+  padding: number = 5,
+  interval: number = 60
+): string {
+  let result = "";
+  for (let i = 0; i < input.length; i++) {
+    result += input[i];
+    if ((i + 1) % interval === 0) {
+      result += "\n" + " ".repeat(padding);
+    }
+  }
+  return result;
+}
+
+function countLeadingSpaces(str: string) {
+  let count = 0;
+  for (let char of str) {
+    if (char === " ") {
+      count++;
+    } else if (char === "\t") {
+      count += 4;
+    } else {
+      break;
+    }
+  }
+  return count;
+}
+
+function Text(props: RETextProps) {
+  return <REText {...props} style={{ ...textColor, ...props.style }} />;
+}
+
+function Hr(props: REHrProps) {
+  return (
+    <REHr
+      {...props}
+      style={{ borderTop: `1px solid ${DIVIDER_COLOR}`, ...props.style }}
+    />
+  );
+}
+
+function SurfaceHr(props: REHrProps) {
+  return (
+    <REHr
+      {...props}
+      style={{
+        borderTop: `1px solid ${SURFACE_DIVIDER_COLOR}`,
+        ...props.style,
+      }}
+    />
+  );
+}
 
 function Fonts({ assetsUrl }: { assetsUrl: string }) {
   return (
@@ -62,6 +214,16 @@ function Fonts({ assetsUrl }: { assetsUrl: string }) {
           format: "woff2",
         }}
         fontWeight="400"
+        fontStyle="normal"
+      />
+      <Font
+        fontFamily="IBM Plex Mono"
+        fallbackFontFamily="mono-space"
+        webFont={{
+          url: `${assetsUrl}/ibm-plex-mono-latin-500.woff2`,
+          format: "woff2",
+        }}
+        fontWeight="500"
         fontStyle="normal"
       />
       <Font
@@ -88,17 +250,83 @@ function Fonts({ assetsUrl }: { assetsUrl: string }) {
   );
 }
 
+type StacktraceContext = {
+  line: string;
+  index: number;
+};
+type StacktraceFrame = {
+  file: string;
+  line?: number;
+  column?: number;
+  important?: boolean;
+  context?: StacktraceContext[];
+};
+
 interface IssueEmailProps {
   url: string;
+  app: string;
   name: string;
+  stage: string;
   message: string;
   assetsUrl: string;
+  stacktrace?: StacktraceFrame[];
+  stacktraceRaw?: string[];
 }
 const IssueEmail = ({
+  app = "console",
+  stage = "production",
   name = "NoSuchBucket",
   assetsUrl = LOCAL_ASSETS_URL,
   message = "The specified bucket does not exist",
   url = "https://console.sst.dev/sst/console/production/issues/pioksmvi6x2sa9zdljvn8ytw",
+  stacktrace = [
+    {
+      file: "node_modules/.pnpm/@smithy+smithy-client@2.1.3/node_modules/@smithy/smithy-client/dist-es/default-error-handler.js",
+      line: 23,
+      column: 17,
+    },
+    {
+      file: "node_modules/.pnpm/@smithy+smithy-client@2.1.3/node_modules/@smithy/smithy-client/dist-es/operation.js",
+      line: 49,
+      column: 28,
+    },
+    {
+      file: "packages/core/src/issue/index.ts",
+      line: 147,
+      column: 35,
+      important: true,
+      context: [
+        {
+          line: "    const key = `stackMetadata/path/that/is/too/long/and/will/overflow/app.${row.app}/stage.${row.stage}/`;",
+          index: 150,
+        },
+        {
+          line: '    console.log("listing", key, "for", bucket);',
+          index: 148,
+        },
+        {
+          line: "    const list = await s3",
+          index: 149,
+        },
+        {
+          line: "      .send(",
+          index: 150,
+        },
+        {
+          line: "        new ListObjectsV2Command({",
+          index: 151,
+        },
+        {
+          line: "          Prefix: key,",
+          index: 152,
+        },
+        {
+          line: "          Bucket: bucket,",
+          index: 153,
+        },
+      ],
+    },
+  ],
 }: IssueEmailProps) => {
   return (
     <Html lang="en">
@@ -115,34 +343,126 @@ const IssueEmail = ({
             <Section>
               <Column>
                 <Img
-                  width="40"
-                  height="31"
+                  height="32"
                   alt="SST Logo"
                   src={`${assetsUrl}/sst-logo.png`}
                 />
               </Column>
               <Column align="right">
-                <Button pX={16} pY={11} style={buttonPrimary} href={url}>
-                  View Issue
+                <Button pX={18} pY={12} style={buttonPrimary} href={url}>
+                  <span style={code}>View Issue</span>
                 </Button>
               </Column>
             </Section>
-            <Text style={code}>
-              <b style={code}>{name}</b>: {message}
-            </Text>
-            <Button
-              pX={20}
-              pY={12}
-              href="https://example.com"
-              style={{ background: "#000", color: "#fff" }}
-            >
-              <b style={{ fontWeight: 600 }}>Click</b> me
-            </Button>
+
+            <Section style={headingHr}>
+              <Hr />
+            </Section>
+
+            <Section>
+              <Text style={{ ...compactText, ...issueBreadcrumb }}>
+                <span>{app}</span>
+                <span style={{ ...code, ...issueBreadcrumbSeparator }}>
+                  &nbsp;/&nbsp;
+                </span>
+                <span>{stage}</span>
+              </Text>
+              <Text style={{ ...issueHeading, ...compactText }}>
+                <Link style={code} href={url}>
+                  {name}
+                </Link>
+              </Text>
+              <Text style={{ ...compactText, ...code }}>{message}</Text>
+            </Section>
+
+            <Section style={{ padding: `${unit * 1.5}px 0 0 0` }}>
+              <Row>
+                <Text style={sectionLabel}>STACK TRACE</Text>
+              </Row>
+              <Section style={stacktraceContainer}>
+                {stacktrace.map((frame, index) => (
+                  <>
+                    <Row key={index}>
+                      <span
+                        style={
+                          frame.important
+                            ? stacktraceFrameFileImportant
+                            : stacktraceFrame
+                        }
+                      >
+                        {frame.file}
+                      </span>
+                      &nbsp;&nbsp;
+                      <span
+                        style={
+                          frame.important
+                            ? stacktraceFramePositionImportant
+                            : stacktraceFrame
+                        }
+                      >
+                        {frame.line}
+                      </span>
+                      <span style={stacktraceFrame}>:</span>
+                      <span
+                        style={
+                          frame.important
+                            ? stacktraceFramePositionImportant
+                            : stacktraceFrame
+                        }
+                      >
+                        {frame.column}
+                      </span>
+                    </Row>
+                    {index < stacktrace.length - 1 && (
+                      <Row>
+                        <SurfaceHr />
+                      </Row>
+                    )}
+                    {frame.context &&
+                      renderStacktraceFrameContext(frame.context)}
+                  </>
+                ))}
+              </Section>
+            </Section>
           </Section>
         </Container>
       </Body>
     </Html>
   );
 };
+
+function renderStacktraceFrameContext(context: StacktraceContext[]) {
+  const minLeadingSpaces = Math.min(
+    ...context.map((row) => countLeadingSpaces(row.line))
+  );
+
+  return (
+    <>
+      <Row>
+        <SurfaceHr />
+      </Row>
+      {context.map((row, index) => (
+        <Row>
+          <pre
+            style={
+              index === 3
+                ? stacktraceFrameContextImportant
+                : stacktraceFrameContext
+            }
+          >
+            {row.index}
+            {"  "}
+            {insertWbr(
+              row.line.substring(minLeadingSpaces),
+              row.index.toString().length +
+                2 +
+                countLeadingSpaces(row.line.substring(minLeadingSpaces))
+            )}
+          </pre>
+        </Row>
+      ))}
+    </>
+  );
+}
 
 export default IssueEmail;
