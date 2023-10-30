7d83069bb3e74f1f8590d02c7cd7c7f92b21a811
Dax Raad <d@ironbay.co>
2023-10-30 17:31:25 -0400

    time gated

--- packages/core/src/workspace/workspace.sql.ts
+++ packages/core/src/workspace/workspace.sql.ts
@@ -1,10 +1,12 @@
 import {
   mysqlTable,
   primaryKey,
+  timestamp,
   uniqueIndex,
   varchar,
 } from "drizzle-orm/mysql-core";
 import { timestamps, id } from "../util/sql";
+import { sql } from "drizzle-orm";
 
 export const workspace = mysqlTable(
   "workspace",
@@ -12,11 +14,14 @@ export const workspace = mysqlTable(
     ...id,
     ...timestamps,
     slug: varchar("slug", { length: 255 }).notNull(),
-    // stripeCustomerID: varchar("stripe_customer_id", { length: 255 }),
-    // stripeSubscriptionID: varchar("stripe_subscription_id", { length: 255 }),
-    // stripeSubscriptionItemID: varchar("stripe_subscription_item_id", {
-    //   length: 255,
-    // }),
+    timeGated: timestamp("time_gated", {
+      mode: "string",
+    }),
+    stripeCustomerID: varchar("stripe_customer_id", { length: 255 }),
+    stripeSubscriptionID: varchar("stripe_subscription_id", { length: 255 }),
+    stripeSubscriptionItemID: varchar("stripe_subscription_item_id", {
+      length: 255,
+    }),
   },
   (table) => ({
     slug: uniqueIndex("slug").on(table.slug),
