db4e912c6c684bac8b5880a3cee6e0b07ebd69d1
Dax Raad <d@ironbay.co>
2023-11-01 18:05:16 -0400

    install sourcemap

--- stacks/issues.ts
+++ stacks/issues.ts
@@ -115,6 +115,9 @@ export function Issues({ stack }: StackContext) {
     environment: {
       EMAIL_DOMAIN: use(DNS).domain,
     },
+    nodejs: {
+      install: ["source-map"],
+    },
   });
 
   bus.subscribe(stack, "app.stage.connected", {
