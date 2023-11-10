abd3774fd3bc7a7a2d458e81e20a7f326544dc29
Frank <wangfanjie@gmail.com>
2023-11-10 11:19:04 -0500

    Add mx record

--- stacks/email.ts
+++ stacks/email.ts
@@ -1,6 +1,6 @@
 import { StackContext, use } from "sst/constructs";
 import { EmailIdentity, Identity } from "aws-cdk-lib/aws-ses";
-import { HostedZone, TxtRecord } from "aws-cdk-lib/aws-route53";
+import { HostedZone, TxtRecord, MxRecord } from "aws-cdk-lib/aws-route53";
 import { DNS } from "./dns";
 
 export function Email(ctx: StackContext) {
@@ -20,6 +20,18 @@ export function Email(ctx: StackContext) {
     recordName: `_dmarc.${dns.zone.zoneName}`,
     zone: dns.zone,
     values: [`v=DMARC1; p=reject; rua=mailto:dmarc@${dns.zone.zoneName};`],
+  });
+
+  new MxRecord(ctx.stack, "mx-record", {
+    recordName: dns.zone.zoneName,
+    zone: dns.zone,
+    values: [
+      { priority: 1, hostName: "ASPMX.L.GOOGLE.COM." },
+      { priority: 5, hostName: "ALT1.ASPMX.L.GOOGLE.COM." },
+      { priority: 5, hostName: "ALT2.ASPMX.L.GOOGLE.COM." },
+      { priority: 10, hostName: "ALT3.ASPMX.L.GOOGLE.COM." },
+      { priority: 10, hostName: "ALT4.ASPMX.L.GOOGLE.COM." },
+    ],
   });
 
   return {
