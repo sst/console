37b24a684c593bc60e1c74d1ef319fc1a6703b65
Dax Raad <d@ironbay.co>
2023-12-05 19:53:20 -0500

    log handler

--- packages/functions/src/events/app-stage-updated.ts
+++ packages/functions/src/events/app-stage-updated.ts
@@ -3,10 +3,15 @@ import { Stage } from "@console/core/app/stage";
 import { AWS } from "@console/core/aws";
 import { EventHandler } from "sst/node/event-bus";
 
-export const handler = EventHandler(Stage.Events.Updated, (evt) =>
+export const handlerInner = EventHandler(Stage.Events.Updated, (evt) =>
   withActor(evt.metadata.actor, async () => {
     const config = await Stage.assumeRole(evt.properties.stageID);
     if (!config) return;
     await Stage.syncMetadata(config);
   })
 );
+
+export const handler = async (evt: any) => {
+  console.log(evt);
+  return handlerInner(evt);
+};
