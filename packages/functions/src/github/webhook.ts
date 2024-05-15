2e3ae5e34587807039c33954f95e60b71be20d55
Frank <frank@sst.dev>
2024-05-15 14:55:01 -0400

    Fix types

--- packages/functions/src/github/webhook.ts
+++ packages/functions/src/github/webhook.ts
@@ -13,15 +13,15 @@ const app = new App({
 
 export const handler = ApiHandler(async (event) => {
   app.webhooks.on("installation.deleted", async (event) => {
-    const orgID = event.payload.installation.account.id;
+    const orgID = event.payload.installation.account!.id;
     await Github.disconnectAll(orgID);
   });
 
   const ret = await app.webhooks.verifyAndReceive({
-    id: useHeader("x-github-delivery"),
+    id: useHeader("x-github-delivery")!,
     name: useHeader("x-github-event") as any,
-    signature: useHeader("x-hub-signature-256"),
-    payload: useBody(),
+    signature: useHeader("x-hub-signature-256")!,
+    payload: useBody()!,
   });
 
   console.log(event);
