f4f4a4482b607085652cfab6be40bf3d7ee9d6c5
Frank <frank@sst.dev>
2024-05-29 18:33:28 -0400

    sync

--- packages/core/migrations/0004_glamorous_songbird.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `run_runner` DROP FOREIGN KEY `workspace_id_aws_account_id_fk`;
---> statement-breakpoint
-ALTER TABLE `run_runner` MODIFY COLUMN `aws_account_id` char(24) NOT NULL;--> statement-breakpoint
-ALTER TABLE `run_runner` ADD CONSTRAINT `workspace_id_aws_account_id_fk` FOREIGN KEY (`workspace_id`,`aws_account_id`) REFERENCES `aws_account`(`workspace_id`,`id`) ON DELETE cascade ON UPDATE no action;
--- packages/core/migrations/0006_hot_sheva_callister.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `run` DROP FOREIGN KEY `workspace_id_stage_id_fk`;--> statement-breakpoint
-ALTER TABLE `run` DROP INDEX `timeCreated`;
