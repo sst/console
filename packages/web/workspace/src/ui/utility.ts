2adf24ee4bf5bcc61c5d1ebb98a35435dc7a3bf8
Dax Raad <d@ironbay.co>
2024-07-07 17:37:06 -0400

    sync

--- packages/web/workspace/src/ui/utility.ts
+++ packages/web/workspace/src/ui/utility.ts
@@ -7,20 +7,20 @@ export const utility = {
       overflow: "hidden",
       whiteSpace: "nowrap",
       textOverflow: "ellipsis",
-    };
+    } as any;
   },
   stack(space: keyof (typeof theme)["space"]) {
     return {
       display: "flex",
       flexDirection: "column",
       gap: theme.space[space],
-    } satisfies CSSProperties;
+    } as any;
   },
   row(space: keyof (typeof theme)["space"]) {
     return {
       display: "flex",
       gap: theme.space[space],
-    };
+    } as any;
   },
 
   text: {
@@ -28,16 +28,16 @@ export const utility = {
       overflow: "hidden",
       whiteSpace: "nowrap",
       textOverflow: "ellipsis",
-    },
+    } as any,
     label: {
       fontWeight: 500,
       letterSpacing: 0.5,
       textTransform: "uppercase",
       fontFamily: theme.font.family.code,
-    },
+    } as any,
     pre: {
       whiteSpace: "pre-wrap",
       overflowWrap: "anywhere",
-    },
+    } as any,
   },
 };
